e98b79f0bdbe7c976406e6b2a836e98793d81661
Sophimp <songfangxi2012@163.com>
2020-03-16 17:45:00 +0800

    add thread and forks examples

--- chapter12.c
+++ chapter12.c
@@ -153,11 +153,85 @@ void pthread_sigwait_test(){
 		err_sys("SIG_SETMASK error");
 	}
 }
+
+pthread_mutex_t lock1 = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t lock2 = PTHREAD_MUTEX_INITIALIZER;
+
+void prepare(){
+	int err;
+
+	printf("preparing lcoks...\n");
+	if ((err = pthread_mutex_lock(&lock1)) != 0){
+		err_cont(err, "can't lock lock1 in prepare handler");
+	}
+	if ((err = pthread_mutex_lock(&lock2)) != 0){
+		err_cont(err, "can't lock lock2 in prepare handler");
+	}
+}
+
+void parent(void){
+	int err;
+
+	printf("parent unlocking locks ... \n");
+	if ((err = pthread_mutex_unlock(&lock1)) != 0){
+		err_cont(err, "can't lock lock1 in parent handler");
+	}
+	if ((err = pthread_mutex_unlock(&lock2)) != 0){
+		err_cont(err, "can't lock lock2 in parent handler");
+	}
+}
+
+void child(void){
+	int err;
+
+	printf("child unlocking locks ... \n");
+	if ((err = pthread_mutex_unlock(&lock1)) != 0){
+		err_cont(err, "can't lock lock1 in child handler");
+	}
+	if ((err = pthread_mutex_unlock(&lock2)) != 0){
+		err_cont(err, "can't lock lock2 in child handler");
+	}
+}
+
+void *thr_fn_fork(void *arg){
+	printf("thread started...\n");
+
+	pause();
+	return 0;
+}
+
+void pthreadForkTest(){
+	int err;
+	pid_t pid;
+	pthread_t tid;
+
+	if ((err = pthread_atfork(prepare, parent, child)) != 0){
+		err_exit(err, "can't install fork handlers");
+	}
+	if ((err = pthread_create(&tid, NULL, thr_fn_fork, 0)) != 0){
+		err_exit(err, "cant' create thread");
+	}
+
+	sleep(2);
+	printf("parent about to fork ...\n");
+
+	if ((pid = fork()) < 0){
+		err_quit("fork failed");
+	}else if(pid == 0){
+		printf("child returned from fork\n");
+	}else{
+		printf("parent returned from fork\n");
+	}
+	exit(0);
+}
+
 int main(void){
 
 	//pthreadSpecificTest();
 
-	pthread_sigwait_test();
+	//pthread_sigwait_test();
+
+	pthreadForkTest();
 
 	return 0;
 }
